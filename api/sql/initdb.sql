5bd46772ddbf6dc5f7f75fb3914db478b2f74e63
Vitor Marcus Dutra <ntw1942@gmail.com>
2024-12-17 14:33:50 -0300

    feature: modelo de usuarios + initdb sql

---
+++ api/sql/initdb.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS network;
+USE network;
+
+DROP TABLE IF EXISTS usuarios;
+
+CREATE TABLE usuarios(
+    id int auto_increment primary key,
+    nome varchar(50) not null,
+    nick varchar(50) not null unique,
+    email varchar(50) not null unique,
+    senha varchar(50) not null,
+    criadoEm timestamp default current_timestamp()
+) ENGINE=INNODB;
