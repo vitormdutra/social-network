b694b1894f3737588e3773545248944b0404443e
Vitor Marcus Dutra <ntw1942@gmail.com>
2024-12-17 14:49:03 -0300

    feature: conexao com banco de dados

---
+++ api/src/banco/banco.go
@@ -0,0 +1,22 @@
+package banco
+
+import (
+	"api/src/config"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql" //Driver
+)
+
+// Conectar aber conexao com o banco
+func Conectar() (*sql.DB, error) {
+	db, err := sql.Open("mysql", config.StringConexcaoBanco)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
